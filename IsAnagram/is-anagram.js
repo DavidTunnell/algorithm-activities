3d3b1fb78be5c27b2ca5a145abd0712095f52464
David Tunnell <davidtunnell9@gmail.com>
2021-08-06 15:37:15 -0500

    solved the 3 algorithms

--- IsAnagram/is-anagram.js
+++ IsAnagram/is-anagram.js
@@ -1,4 +1,24 @@
 // Write code to create a function that accepts two strings
 // Return true if the strings are anagrams of each other, otherwise return false
+// var strA = "octagonal";
+// var strB = "tganoalco"; //true
 
-var isAnagram = function(strA, strB) {};
+var isAnagram = function(strA, strB) {
+    // get an array with no spaces and all lower case
+    strA = strA.replace(/\s/g, "").toLowerCase();
+    strB = strB.replace(/\s/g, "").toLowerCase().split("");
+    // if not the same number of characters then it's not an anagram
+    if (strA.length != strB.length) {
+        return false;
+    }
+    // iterate through each value in strA and filter it out of strB
+    for (let i = 0; i < strA.length; i++) {
+        strB = strB.filter(item => item !== strA[i]);
+    }
+    //if strB is empty after all of that it's an anagram
+    if (strB.length === 0) {
+        return true;
+    }
+    //else it is not
+    return false;
+};
--- IsUnique/is-unique.js
+++ IsUnique/is-unique.js
@@ -1,4 +1,14 @@
 // Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
-
-var isUnique = function(arr) {};
+// [2, 8, 7, 5, 4, 3]
+var isUnique = function(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        //i+1 so the same comparisons aren't repeatedly made
+        for (let f = (i + 1); f < arr.length; f++) {
+            if (arr[i] === arr[f]) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
