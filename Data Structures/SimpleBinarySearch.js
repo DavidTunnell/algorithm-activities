34088e523bd689e4d0ae39e9089ad21f2e362e41
David Tunnell <davidtunnell9@gmail.com>
2021-10-02 10:17:18 -0500

    added simple binary search algo

---
+++ Data Structures/SimpleBinarySearch.js
@@ -0,0 +1,21 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 15];
+
+function binarySearch(array, element) {
+    let start = 0;
+    let end = array.length - 1;
+
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+
+        if (array[mid] === element) {
+            return mid;
+        } else if (array[mid] < element) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return -1;
+}
+console.log(binarySearch(arr, 12));
+console.log(binarySearch(arr, 13));
